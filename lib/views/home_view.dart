9f6de4185b3b7548a95ec2f4974809bdf3e4ba9d
Riyam224 <cs50riyam221@gmail.com>
2024-08-28 12:02:30 +0300

    add smooth page indicator to move smoothly between pages

---
+++ lib/views/home_view.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final pageController = PageController();
+  var currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: pageController,
+              children: const [
+                Page1(),
+                Page2(),
+                Page3(),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(),
+                  backgroundColor: Colors.blue,
+                  minimumSize: const Size(200, 60),
+                ),
+                onPressed: () {
+                  setState(() {
+                    currentPage += 1;
+                  });
+                  pageController.jumpToPage(currentPage);
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Next',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Icon(
+                      Icons.chevron_right,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ],
+                )),
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          SmoothPageIndicator(
+            controller: pageController,
+            count: 3,
+            effect: const WormEffect(
+              dotHeight: 20,
+              dotWidth: 20,
+              dotColor: Colors.grey,
+              activeDotColor: Colors.blue,
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.blue[200],
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.red[200],
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  const Page3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.green[200],
+    );
+  }
+}
